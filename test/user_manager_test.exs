59f904fd89475d5dc7929c70fd7ac6021c5a72bd
Matheus C Marques <matheuscamarques@gmail.com>
2024-03-31 02:44:37 -0300

    fix: refactor this tests using new Factory pattern.

--- test/user_manager_test.exs
+++ test/user_manager_test.exs
@@ -1,19 +1,17 @@
 defmodule UserManagerTest do
+  alias UserManager.Factory
   use ExUnit.Case, async: false
   doctest UserManager
 
   test "create unique user with all procedutes" do
-    user_payload_schema = %{
-      name: "Fulano de tal",
-      email: "fulano@email.com",
-      password: "password"
-    }
+    user_payload_schema = Factory.data(:user)
 
     {:ok, created_user} = UserManager.create_user(user_payload_schema)
     {:ok, user} = UserManager.get_user(created_user.email)
 
-    assert user.name == "Fulano de tal"
-    assert user.email == "fulano@email.com"
+    assert user.name == user_payload_schema.name
+    assert user.email == user_payload_schema.email
+    assert user.password != user_payload_schema.password
     assert user.password != nil
     assert user.id != nil
 
@@ -23,11 +21,7 @@ defmodule UserManagerTest do
   end
 
   test "create two users with identity emails returning error" do
-    user_payload_schema = %{
-      name: "Fulano de tal",
-      email: "fulano2@email.com",
-      password: "password"
-    }
+    user_payload_schema = Factory.data(:user)
 
     UserManager.create_user(user_payload_schema)
     assert {:error, :user_already_registred} == UserManager.create_user(user_payload_schema)
